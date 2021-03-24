3e9813506b330f330df8eaf1892db4e11af02332
214254151 <abongiletshopi@gmail.com>
2021-03-24 23:26:41 +0200

    externded the funcionalities

--- src/za/ac/cput/Calculator.java
+++ src/za/ac/cput/Calculator.java
@@ -2,7 +2,13 @@ package za.ac.cput;
 
 public class Calculator {
 
+    /**
+     * fixixng the length
+     * @param str
+     * @return
+     */
     public int lengthOfString(String str){
+        if(str == null || str.isEmpty()) return 0;
         return str.length();
     }
 }
